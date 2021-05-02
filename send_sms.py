429df1ef7a5dd792d227442c15f16063934a4188
pmanlukas <pmanlukas@github.com>
2021-05-02 21:00:08 +0200

    added send sms utility

---
+++ send_sms.py
@@ -0,0 +1,18 @@
+import os
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+    .create(
+         body='This is the ship that made the Kessel Run in fourteen parsecs?',
+         from_='+17372103868',
+         to='+491709296197'
+     )
+
+print(message.sid)
